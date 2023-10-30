c0588f84a589de11ce1e7b55e6396fbc914a03d4
MFDD1 <judas.sanchez1@icloud.com>
2023-10-30 11:46:04 -0700

    version2

--- src/App.js
+++ src/App.js
@@ -16,13 +16,25 @@ const App = () => {
     "?",
   ])
 
+  const handleSquareClick = (clickedSquareIndex) => {
+    let updatedBoard = [...board]
+    updatedBoard[clickedSquareIndex] = " w "
+    setBoard(updatedBoard)
+  }
+
   return (
     <>
       <h1>Treasure Hunt Game</h1>
       <div className="board"> 
       {board.map((value, index) => {
         console.log(value, index)
-        return <Square value={value} />
+        return ( 
+        <Square 
+        value={value}
+        index ={index}
+        handleSquareClick={handleSquareClick} 
+        />
+        )
       })}
       </div>
     </>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,14 +1,18 @@
 import React from "react"
 
-const Square = ({value, index}) => {
+const Square = ({value, index, handleSquareClick}) => {
+  const handleClick = () => {
+    handleSquareClick(index)
+  }
+
   // destructuring props
   // const { value } = props; is the same as props.value
   // instead pass the destructed calue through
 
   return (
     <>
-      <div className="square">{value}</div>
-      
+      <div className="square" onClick={handleClick}>{value}
+      </div>
     </>
   )
 }
